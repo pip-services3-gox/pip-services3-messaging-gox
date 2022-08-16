07a9a8b48d4230d1dde360c59fb8a0f6fbebd95b
Dmitry Krayniy <krdima92@gmail.com>
2022-08-16 17:56:06 +0400

    Updated pipeline

--- clean.ps1
+++ clean.ps1
@@ -1,22 +1,24 @@
 #!/usr/bin/env pwsh
 
-# Recreate image names using the data in the "component.json" file
-$component = Get-Content -Path "component.json" | ConvertFrom-Json
-$docsImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-docs"
-$testImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-test"
-
-# Clean up build directories
-Get-ChildItem -Path "." -Include "exe" -Recurse | foreach($_) { Remove-Item -Force -Recurse $_.FullName }
+# Recreate image names using the data in the "$PSScriptRoot/component.json" file
+$component = Get-Content -Path "$PSScriptRoot/component.json" | ConvertFrom-Json
+$testImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)-test"
+$docsImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)-docs"
 
 # Remove docker images
 docker rmi $docsImage --force
 docker rmi $testImage --force
-docker image prune --force
 docker rmi -f $(docker images -f "dangling=true" -q) # remove build container if build fails
+docker image prune --force
 
 # Remove existed containers
 $exitedContainers = docker ps -a | Select-String -Pattern "Exit"
-foreach($c in $exitedContainers) { docker rm $c.String().Split(" ")[0] }
+foreach($c in $exitedContainers) { docker rm $c.ToString().Split(" ")[0] }
 
 # Remove unused volumes
 docker volume rm -f $(docker volume ls -f "dangling=true")
+
+# Clean up build directories
+if (Test-Path -Path "$PSScriptRoot/exe") {
+    Remove-Item -Recurse -Force "$PSScriptRoot/exe"
+}
